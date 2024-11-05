9773cd3e9f63f4a7625408ce68adf0eea919e366
mariaeduardabobio <128700687+mariaeduardabobio@users.noreply.github.com>
2024-11-05 07:54:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Futebol',
+    'Quantos jogadores compoẽm um time de futebol?',
+    'É necessário que se tenha 11 jogadores em cada time'
+)
+
+criaCartao(
+    'Futebol',
+    'Como funciona o tempo das partidas?',
+    'Possui 2 tempos de 45 minutos cada, e entre eles um intervalo de 15 minutos'
+)
+
+criaCartao(
+    'Futebol',
+    'Quais são as posições existentes?',
+    'Goleiro, zagueiro, lateral, meia e atacante'
+)
+
+criaCartao(
+    'Futebol',
+    'Quais competições os times brasileiros da série A podem participar, se classificados?',
+    'Brasileirão série A, Campeonato Estadual, Copa do Brasil, Sulamericana, Libertadores e Munial'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas rodadas são disputadas no campeonato brasileiro?',
+    'São 38 rodadas'
+)
+criaCartao(
+    'Matemática',
+    'O que diz o teorema de pitágoras?',
+    'Em todo triângulo retângulo a soma dos quadrados dos catetos é igual ao quadrado da hipotenusa'
+)
